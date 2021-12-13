06f20d3d88e5f979ed0449a6b8290ab715535907
Пугачев Антон Александрович <APugachev@inno.local>
2021-12-13 11:37:14 +0300

    fix: [main] fix library bugs and build bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from './router/Router';
 import { routes } from './router/config';
 import { Notifications } from 'juicyfront';
 
 function App() {
+  useEffect(() => {
+    console.warn('================================================================');
+    console.log(`%c Active application is ${process.env.REACT_APP_ENV}`, 'color:DodgerBlue;font-size:large');
+    console.log(`%c ${process.env.REACT_APP_V}`, 'color:red;font-size:large');
+    console.warn('================================================================');
+
+  }, []);
   return (
     <div id={ process.env.REACT_APP_NAME }>
       <Router routes={ routes }/>
--- webpack.config.local.js
+++ webpack.config.local.js
@@ -1,11 +1,22 @@
-const common=require('./webpack.config.common')
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const conf= common.config
+// const common=require('./webpack.config.common')
+// const HtmlWebPackPlugin = require("html-webpack-plugin");
+// const conf= common.config
+//
+// conf.plugins.push(new HtmlWebPackPlugin({
+//     template: "./public/indexLocal.html",
+//     favicon: "./public/favicon.ico",
+// }),)
+//
+//
+// module.exports = conf
+const common=require('./node_modules/juicyfront/wp5_template')
 
-conf.plugins.push(new HtmlWebPackPlugin({
-    template: "./public/indexLocal.html",
-    favicon: "./public/favicon.ico",
-}),)
-
+const conf= common.config({
+    port:8888,
+    applicationName:'meetings',
+    russianName:'Meetings',
+    exposes: {},
+    baseUrl:'/'
+})
 
 module.exports = conf
