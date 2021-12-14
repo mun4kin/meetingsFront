dfaf48c48050b2fc6eec73d107d125c150b6fd5e
Пугачев Антон Александрович <APugachev@inno.local>
2021-12-14 19:07:53 +0300

    fix: [main] fix tests

--- src/components/organisms/MeetingCard/MeetingCard.spec.tsx
+++ src/components/organisms/MeetingCard/MeetingCard.spec.tsx
@@ -6,17 +6,7 @@ import moment from "moment";
 import userEvent from "@testing-library/user-event";
 import {byRole, byText} from "testing-library-selector";
 
-
-
-
 describe('Test <MeetingCard/> component', () => {
-    // turning off warnings
-    //======================================================
-    const originalError = console.error
-     afterAll(() => (console.error = originalError))
-    let consoleOutput:any[] = []
-    const mockedError = (output:any) => consoleOutput.push(output)
-    beforeEach(() => (console.error = mockedError))
     //======================================================
    const  onChange=jest.fn()
     const setConfirmModal=jest.fn()
@@ -41,44 +31,40 @@ describe('Test <MeetingCard/> component', () => {
     //------------------------------------------------------------------------------------------------------------------
     it('Check menu and mouseEnter', async () =>  {
         //we have portal element since it must be rendered with { container: document.body }
-        const {container} =render(<MeetingCard data={meetingMock} setConfirmModal={setConfirmModal} isCreater={true} onChange={onChange}/>,
-            { container: document.body });
+        const {container} =render(<MeetingCard data={meetingMock} setConfirmModal={setConfirmModal} isCreater={true} onChange={onChange}/>);
 
         expect(container.getElementsByClassName('menu__wrapper')).toHaveLength(1)
         expect(screen.getByTestId('rf-menu')).toBeInTheDocument();
-
         fireEvent.mouseEnter(container.getElementsByClassName('rf-avatar-status')[0])
         await waitFor(() =>{
             expect(screen.getByTestId('email')).toBeTruthy();
         })
-
         userEvent.click(byRole("button").get())
         await waitFor(() =>{
-            expect(getByText(container, 'Delete')).toBeTruthy();
-            expect(getByText(container, 'Edit')).toBeTruthy();
+            expect(screen.queryByText('Delete')).toBeTruthy();
+            expect(screen.queryByText('Edit')).toBeTruthy();
         })
-        userEvent.click(byText("Delete").get())
+      userEvent.click(screen.getAllByText('Delete')[0])
         expect(setConfirmModal).toHaveBeenCalled();
         userEvent.click(byRole("button").get())
 
         await waitFor(() =>{
-            expect(getByText(container, 'Delete')).toBeTruthy();
-            expect(getByText(container, 'Edit')).toBeTruthy();
+            expect(screen.queryByText('Delete')).toBeTruthy();
+            expect(screen.queryByText('Edit')).toBeTruthy();
         })
-        userEvent.click(byText("Edit").get())
+        userEvent.click(screen.getAllByText('Edit')[0])
         expect(onChange).toHaveBeenCalled();
     })
     //------------------------------------------------------------------------------------------------------------------
     it('Check disabled menu', async () =>  {
-        const {container} =render(<MeetingCard data={meetingMock} setConfirmModal={setConfirmModal} isCreater={false} onChange={onChange}/>,
-            { container: document.body });
+        render(<MeetingCard data={meetingMock} setConfirmModal={setConfirmModal} isCreater={false} onChange={onChange}/>);
         userEvent.click(byRole("button").get())
         await waitFor(() =>{
-            expect(getByText(container, 'Delete')).toBeTruthy();
-            expect(getByText(container, 'Edit')).toBeTruthy();
+            expect(screen.queryByText('Delete')).toBeInTheDocument();
+            expect(screen.queryByText('Edit')).toBeInTheDocument();
         })
-        expect(() =>      userEvent.click(byText("Delete").get())).toThrow()
-        expect(() =>      userEvent.click(byText("Edit").get())).toThrow()
+        expect(() =>userEvent.click(screen.getAllByText('Delete')[0])).toThrow()
+        expect(() =>userEvent.click(screen.getAllByText('Edit')[0])).toThrow()
     })
 
 })
