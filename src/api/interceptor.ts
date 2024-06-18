82b143ec875c46fb86b8aa95c237afc3290739ad
Anton Pugachev <anton.pugachev@bluescape.com>
2024-06-18 16:27:10 -0400

    fix: [main] fix

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -8,7 +8,7 @@ const intercept = () => {
   Axios.interceptors.request.use((config) => {
 
     const host = ~window.location.hostname.indexOf('127.0.') ||
-    ~window.location.hostname.indexOf('localhost') ? server : 'https://meetings-back-new.herokuapp.com';
+    ~window.location.hostname.indexOf('localhost') ? server : 'https://meetingsback.onrender.com/';
     config.url = host + config.url;
 
     return config;
